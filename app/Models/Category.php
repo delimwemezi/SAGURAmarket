6febbe3426d02fdda5941c63501b30b486bc2641
delimwemezi <delimwemezi@gmail.com>
2026-06-05 16:58:09 +0300

    modified:   app/Models/Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,22 +1,10 @@
 <?php
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
 {
-    public function up(): void
-    {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->timestamps();
-        });
-    }
-
-    public function down(): void
-    {
-        Schema::dropIfExists('categories');
-    }
-};
+    //
+}
