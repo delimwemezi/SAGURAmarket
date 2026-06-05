f79c4fd3299218ee97ea679fafd97a92ece1482c
delimwemezi <delimwemezi@gmail.com>
2026-06-05 16:50:20 +0300

    new file:   app/Models/Payment.php 	modified:   app/Models/Product.php 	new file:   database/migrations/2026_06_05_134952_create_payments_table.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'price',
+        'stock',
+        'image',
+        'model_3d'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
