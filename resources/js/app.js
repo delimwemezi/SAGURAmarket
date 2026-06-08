8e69ce445ffa0c1e937afb4d4db2a5666333d9dc
delimwemezi <delimwemezi@gmail.com>
2026-06-08 13:12:33 +0300

    modified:   resources/js/app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+const routes = {
+    '/':          renderHomePage,
+    '/login':     renderLoginPage,
+    '/register':  renderRegisterPage,   // <-- new
+    '/dashboard': renderDashboard,
+    '/super-admin': renderSuperAdmin,
+};
